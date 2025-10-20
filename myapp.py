4efb5c2d2838bf4c367f722cd117e147f559e41a
高橋 俊文 <toshifumi.takahashi@dynmc.co.jp>
2025-10-20 14:00:20 +0900

    Add automatic database initialization

--- myapp.py
+++ myapp.py
@@ -170,4 +170,6 @@ def logout():
     return redirect('login')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)    
